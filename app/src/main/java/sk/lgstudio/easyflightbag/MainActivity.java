95b0233c60f5d9f498b7827cdad2a2b7b05e0700
Gabriel <xlehoc01@stud.fit.vutbr.cz>
2016-09-08 10:18:20 +0200

    Initial commit

---
+++ app/src/main/java/sk/lgstudio/easyflightbag/MainActivity.java
@@ -0,0 +1,13 @@
+package sk.lgstudio.easyflightbag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
