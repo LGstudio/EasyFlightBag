35ded0d65b00c8c5e36198274c05ae493e202b86
LGstudio <xlehoc01@stud.fit.vutbr.vz>
2017-04-07 12:23:48 +0200

    BT now running in thread

--- app/src/main/java/sk/lgstudio/easyflightbag/services/BTTrackerService.java
+++ app/src/main/java/sk/lgstudio/easyflightbag/services/BTTrackerService.java
@@ -7,39 +7,40 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.UUID;
 
 import sk.lgstudio.easyflightbag.R;
 
 /**
- * Created by LGstudio on 2017-04-03.
+ * Service to recieve GPS data from a Bluetooth source
  */
 
 public class BTTrackerService extends Service {
 
-    private static final UUID MY_UUID =
-            UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
+    // Bluetooth service identification
+    private static final UUID MY_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
 
-    // hardcode your server's MAC address here
-    // private static String address = "34:E6:AD:5A:25:66";
+    private BTThread thread;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        (new GetBTData()).execute();
+
+        thread = new BTThread();
+        thread.start();
     }
 
     @Override
     public void onDestroy() {
+        thread.run = false;
         super.onDestroy();
     }
 
@@ -49,20 +50,27 @@ public class BTTrackerService extends Service {
         return null;
     }
 
+    public Context getServiceContext(){
+        return getApplicationContext();
+    }
 
-    public void sendLocation(Location mLastLocation) {
+    /**
+     * Send the location via intent for the activity
+     * @param data [lon, lat, alt. bearing, speed]
+     */
+    private void sendLocation(String[] data) {
 
         Context context = getApplicationContext();
         Intent intent = new Intent(context.getString(R.string.gps_intent_filter));
-        if (mLastLocation != null) {
+        if (data.length == 5) {
 
-            intent.putExtra(context.getString(R.string.gps_latitude), mLastLocation.getLatitude());
-            intent.putExtra(context.getString(R.string.gps_longitude), mLastLocation.getLongitude());
-            intent.putExtra(context.getString(R.string.gps_accuracy), mLastLocation.getAccuracy());
-            intent.putExtra(context.getString(R.string.gps_bearing), mLastLocation.getBearing());
-            intent.putExtra(context.getString(R.string.gps_speed), mLastLocation.getSpeed());
-            intent.putExtra(context.getString(R.string.gps_time), mLastLocation.getTime());
-            intent.putExtra(context.getString(R.string.gps_altitude), mLastLocation.getAltitude());
+            intent.putExtra(context.getString(R.string.gps_latitude), Float.parseFloat(data[1]));
+            intent.putExtra(context.getString(R.string.gps_longitude), Float.parseFloat(data[0]));
+            intent.putExtra(context.getString(R.string.gps_accuracy), 0f);
+            intent.putExtra(context.getString(R.string.gps_bearing), Float.parseFloat(data[3]));
+            intent.putExtra(context.getString(R.string.gps_speed), Float.parseFloat(data[4]));
+            intent.putExtra(context.getString(R.string.gps_time), 0f);
+            intent.putExtra(context.getString(R.string.gps_altitude), Float.parseFloat(data[2]));
             intent.putExtra(getString(R.string.gps_enabled), true);
         }
         else {
@@ -71,57 +79,42 @@ public class BTTrackerService extends Service {
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
-    private class GetBTData extends AsyncTask<Void, Void, Void>{
+    private class BTThread extends Thread {
+
+        boolean run = true;
 
         @Override
-        protected Void doInBackground(Void... params) {
-
+        public void run() {
             BluetoothSocket socket = null;
             BluetoothAdapter mAdapter = BluetoothAdapter.getDefaultAdapter();
 
-            Log.e("BT", "Waiting...");
-
             try {
-                BluetoothServerSocket mmServerSocket = mAdapter.listenUsingRfcommWithServiceRecord("MyService", MY_UUID);
+                BluetoothServerSocket mmServerSocket = mAdapter.listenUsingRfcommWithServiceRecord("EFB", MY_UUID);
                 socket = mmServerSocket.accept();
-            } catch(Exception e){}
+            } catch(Exception e){
+                return;
+            }
 
             byte[] buffer = new byte[256];  // buffer store for the stream
             int bytes; // bytes returned from read()
             try {
 
-                InputStream tmpIn = null;
-
                 // Get the BluetoothSocket input stream
-                tmpIn = socket.getInputStream();
+                InputStream tmpIn = socket.getInputStream();
 
-                while (true){
+                while (run){
                     if (tmpIn != null){
                         DataInputStream mmInStream = new DataInputStream(tmpIn);
 
                         // Read from the InputStream
                         bytes = mmInStream.read(buffer);
                         String readMessage = new String(buffer, 0, bytes);
-
-                        String[] data = readMessage.split(" ");
-
-                        Location loc = new Location("BT");
-                        loc.setAccuracy(0f);
-                        loc.setAltitude(Float.parseFloat(data[2]));
-                        loc.setLongitude(Float.parseFloat(data[0]));
-                        loc.setLatitude(Float.parseFloat(data[1]));
-                        loc.setBearing(Float.parseFloat(data[3]));
-                        loc.setSpeed(Float.parseFloat(data[4]));
-
-                        sendLocation(loc);
+                        sendLocation(readMessage.split(" "));
                     }
                 }
 
             } catch (Exception e) {}
 
-            Log.e("BT", "Failed. Please reset");
-
-            return null;
         }
     }
 }
